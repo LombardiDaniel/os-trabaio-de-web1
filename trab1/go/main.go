0179c5852af131cf48d1dfa2a70812448d00f8b5
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-05-16 09:09:46 -0300

    examples

--- trab1/go/main.go
+++ trab1/go/main.go
@@ -5,26 +5,27 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/lombardidaniel/os-trab-de-web1/trab1/go/internal/handlers"
 	"github.com/lombardidaniel/os-trab-de-web1/trab1/go/pkg/common"
 	"github.com/lombardidaniel/os-trab-de-web1/trab1/go/pkg/rest"
 )
 
-type A struct {
-	Name string `json:"name"`
-}
+var mux *http.ServeMux
 
 func init() {
 	common.InitSlogger()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		rest.JSON(w, http.StatusOK, A{Name: "Daniel"})
-		fmt.Print("hi")
+	mux = http.NewServeMux()
+	handlers.RegisterExamples(mux)
+
+	mux.HandleFunc("GET /healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		rest.String(w, http.StatusOK, "OK")
 	})
 }
 
 func main() {
 	slog.Info("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		slog.Error(fmt.Sprintf("Error starting server: %s", err.Error()))
 	}
 }
