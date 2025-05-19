e11fec503db7f89ff0cc70dee4eea9ea745046a6
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-05-18 22:13:44 -0300

    auth

---
+++ trab1/go/pkg/constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+var (
+	AppName string = "quack-app"
+)
---
+++ trab1/go/pkg/rest/recieve.go
@@ -0,0 +1,24 @@
+package rest
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/lombardidaniel/os-trab-de-web1/trab1/go/pkg/constants"
+)
+
+var authCookieName = constants.AppName + "-userSessionId"
+
+func UserSessionId(req *http.Request) (string, error) {
+	c, err := req.Cookie(authCookieName)
+	if err != nil {
+		return "", err
+	}
+
+	if c.Expires.After(time.Now()) {
+		return "", errors.New("cookie expired")
+	}
+
+	return c.String(), nil
+}
