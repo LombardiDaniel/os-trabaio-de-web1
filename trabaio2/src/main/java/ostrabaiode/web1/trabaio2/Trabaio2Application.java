9ea254f56c6fd3d6b86367a43da81aa8c77fde77
Arthur <thursilverio@gmail.com>
2025-06-15 03:07:03 -0300

    first commit trabaio 2

---
+++ trabaio2/src/main/java/ostrabaiode/web1/trabaio2/Trabaio2Application.java
@@ -0,0 +1,13 @@
+package ostrabaiode.web1.trabaio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabaio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabaio2Application.class, args);
+	}
+
+}
