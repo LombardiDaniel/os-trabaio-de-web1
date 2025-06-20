5bb4fa4e9d78626e0042900caecaa4393f186e67
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-06-20 11:51:52 -0300

    fix: models now work correctly with join tables

--- trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/controllers/TesterController.java
+++ trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/controllers/TesterController.java
@@ -1,130 +1,130 @@
-package com.aa2.GamePlatform.controllers;
-
-import com.aa2.GamePlatform.models.Tester;
-import com.aa2.GamePlatform.models.TesterDto;
-import com.aa2.GamePlatform.repositories.TesterRepository;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-
-@Controller
-@RequestMapping({"tester", "testers"})
-public class TesterController {
-    @Autowired
-    private TesterRepository testerRepository;
-
-    @GetMapping({"","/"})
-    public String tester(Model model) {
-        model.addAttribute("testers", testerRepository.findAll());
-
-        return "tester/index";
-    }
-
-    @GetMapping("/create")
-    public String createTester(Model model) {
-        TesterDto tester = new TesterDto();
-        model.addAttribute("tester", tester);
-
-        return "tester/create";
-    }
-
-    @PostMapping("/create")
-    public String createTester(@Valid @ModelAttribute TesterDto tester,
-                               BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return "tester/create";
-        }
-
-        Tester createdTester = new Tester();
-        createdTester.setFirstName(tester.getFirstName());
-        createdTester.setLastName(tester.getLastName());
-        createdTester.setEmail(tester.getEmail());
-        createdTester.setUserAdmin(tester.getUserAdmin());
-        createdTester.setCreatedAt(new Date().toInstant());
-        createdTester.setUpdatedAt(new Date().toInstant());
-
-        testerRepository.save(createdTester);
-
-        return "redirect:/tester";
-    }
-
-    @GetMapping("/edit")
-    public String editTester(Model model, @RequestParam int id) {
-
-        Tester testerToEdit = testerRepository.findById(id).orElse(null);
-
-        if (testerToEdit == null) {
-            return "redirect:/tester";
-        }
-
-        TesterDto updatedTester = new TesterDto();
-        updatedTester.setFirstName(testerToEdit.getFirstName());
-        updatedTester.setLastName(testerToEdit.getLastName());
-        updatedTester.setEmail(testerToEdit.getEmail());
-        updatedTester.setUserAdmin(testerToEdit.getUserAdmin());
-
-        model.addAttribute("tester", testerToEdit);
-        model.addAttribute("testerDto", updatedTester);
-
-        return "tester/edit";
-    }
-
-    @PostMapping("/edit")
-    public String editTester(Model model,
-                             @RequestParam int id,
-                             @Valid @ModelAttribute TesterDto testerDto,
-                             BindingResult bindingResult) {
-
-        Tester testerToEdit = testerRepository.findById(id).orElse(null);
-        if (testerToEdit == null) {
-            return "redirect:/tester";
-        }
-
-        model.addAttribute("tester", testerToEdit);
-
-        if (bindingResult.hasErrors()) {
-            return "tester/edit";
-        }
-
-        testerToEdit.setFirstName(testerDto.getFirstName());
-        testerToEdit.setLastName(testerDto.getLastName());
-        testerToEdit.setEmail(testerDto.getEmail());
-        testerToEdit.setUserAdmin(testerDto.getUserAdmin());
-        testerToEdit.setUpdatedAt(new Date().toInstant());
-
-        try {
-            testerRepository.save(testerToEdit);
-        }
-        catch (Exception e) {
-            bindingResult.addError(new FieldError(
-                    "testerDto",
-                    "email",
-                    testerDto.getEmail(),
-                    false, null, null,
-                    "Email Already in use")
-            );
-            return "tester/edit";
-        }
-
-        return "redirect:/tester";
-    }
-
-    @GetMapping("/delete")
-    public String deleteTester(Model model, @RequestParam int id) {
-
-        Tester testerToDelete = testerRepository.findById(id).orElse(null);
-
-        if (testerToDelete != null) {
-            testerRepository.delete(testerToDelete);
-        }
-
-        return "redirect:/tester";
-    }
-}
+//package com.aa2.GamePlatform.controllers;
+//
+//import com.aa2.GamePlatform.models.Tester;
+//import com.aa2.GamePlatform.models.TesterDto;
+//import com.aa2.GamePlatform.repositories.TesterRepository;
+//import jakarta.validation.Valid;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Date;
+//
+//@Controller
+//@RequestMapping({"tester", "testers"})
+//public class TesterController {
+//    @Autowired
+//    private TesterRepository testerRepository;
+//
+//    @GetMapping({"","/"})
+//    public String tester(Model model) {
+//        model.addAttribute("testers", testerRepository.findAll());
+//
+//        return "tester/index";
+//    }
+//
+//    @GetMapping("/create")
+//    public String createTester(Model model) {
+//        TesterDto tester = new TesterDto();
+//        model.addAttribute("tester", tester);
+//
+//        return "tester/create";
+//    }
+//
+//    @PostMapping("/create")
+//    public String createTester(@Valid @ModelAttribute TesterDto tester,
+//                               BindingResult bindingResult) {
+//
+//        if (bindingResult.hasErrors()) {
+//            return "tester/create";
+//        }
+//
+//        Tester createdTester = new Tester();
+//        createdTester.setFirstName(tester.getFirstName());
+//        createdTester.setLastName(tester.getLastName());
+//        createdTester.setEmail(tester.getEmail());
+//        createdTester.setUserAdmin(tester.getUserAdmin());
+//        createdTester.setCreatedAt(new Date().toInstant());
+//        createdTester.setUpdatedAt(new Date().toInstant());
+//
+//        testerRepository.save(createdTester);
+//
+//        return "redirect:/tester";
+//    }
+//
+//    @GetMapping("/edit")
+//    public String editTester(Model model, @RequestParam int id) {
+//
+//        Tester testerToEdit = testerRepository.findById(id).orElse(null);
+//
+//        if (testerToEdit == null) {
+//            return "redirect:/tester";
+//        }
+//
+//        TesterDto updatedTester = new TesterDto();
+//        updatedTester.setFirstName(testerToEdit.getFirstName());
+//        updatedTester.setLastName(testerToEdit.getLastName());
+//        updatedTester.setEmail(testerToEdit.getEmail());
+//        updatedTester.setUserAdmin(testerToEdit.getUserAdmin());
+//
+//        model.addAttribute("tester", testerToEdit);
+//        model.addAttribute("testerDto", updatedTester);
+//
+//        return "tester/edit";
+//    }
+//
+//    @PostMapping("/edit")
+//    public String editTester(Model model,
+//                             @RequestParam int id,
+//                             @Valid @ModelAttribute TesterDto testerDto,
+//                             BindingResult bindingResult) {
+//
+//        Tester testerToEdit = testerRepository.findById(id).orElse(null);
+//        if (testerToEdit == null) {
+//            return "redirect:/tester";
+//        }
+//
+//        model.addAttribute("tester", testerToEdit);
+//
+//        if (bindingResult.hasErrors()) {
+//            return "tester/edit";
+//        }
+//
+//        testerToEdit.setFirstName(testerDto.getFirstName());
+//        testerToEdit.setLastName(testerDto.getLastName());
+//        testerToEdit.setEmail(testerDto.getEmail());
+//        testerToEdit.setUserAdmin(testerDto.getUserAdmin());
+//        testerToEdit.setUpdatedAt(new Date().toInstant());
+//
+//        try {
+//            testerRepository.save(testerToEdit);
+//        }
+//        catch (Exception e) {
+//            bindingResult.addError(new FieldError(
+//                    "testerDto",
+//                    "email",
+//                    testerDto.getEmail(),
+//                    false, null, null,
+//                    "Email Already in use")
+//            );
+//            return "tester/edit";
+//        }
+//
+//        return "redirect:/tester";
+//    }
+//
+//    @GetMapping("/delete")
+//    public String deleteTester(Model model, @RequestParam int id) {
+//
+//        Tester testerToDelete = testerRepository.findById(id).orElse(null);
+//
+//        if (testerToDelete != null) {
+//            testerRepository.delete(testerToDelete);
+//        }
+//
+//        return "redirect:/tester";
+//    }
+//}
--- trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/models/Project.java
+++ trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/models/Project.java
@@ -9,16 +9,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "projects")
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "projectname")
     private String projectName;
 
-    @Column(name = "projectdescription")
     private String projectDescription;
 
     @CreationTimestamp
@@ -27,64 +26,11 @@ public class Project {
     @UpdateTimestamp
     private Instant updatedAt;
 
-    @ManyToMany(mappedBy = "projects")
-    private Set<TestSession> testSessions = new HashSet<>();
-
-    public Project(Integer id, String projectName, String projectDescription, Instant createdAt, Instant updatedAt) {
-        this.id = id;
-        this.projectName = projectName;
-        this.projectDescription = projectDescription;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public Project() {};
-
-    public Set<TestSession> getTestSessions() {
-        return testSessions;
-    }
-
-    public void setTestSessions(Set<TestSession> testSessions) {
-        this.testSessions = testSessions;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getProjectDescription() {
-        return projectDescription;
-    }
-
-    public void setProjectDescription(String projectDescription) {
-        this.projectDescription = projectDescription;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "project_tester",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "tester_id")
+    )
+    private Set<Tester> testers = new HashSet<>();
 }
