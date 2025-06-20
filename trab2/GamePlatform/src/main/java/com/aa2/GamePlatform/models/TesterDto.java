9fe4fa705de57351fb3b1f4ca5f3c8c406a94fb7
Arthur <thursilverio@gmail.com>
2025-06-20 02:00:39 -0300

    add: full crud tester

--- trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/models/TesterDto.java
+++ trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/models/TesterDto.java
@@ -1,12 +1,8 @@
 package com.aa2.GamePlatform.models;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.Instant;
+import jakarta.validation.constraints.NotNull;
 
 public class TesterDto {
     @NotEmpty(message = "Cannot empty, first name required")
@@ -19,6 +15,38 @@ public class TesterDto {
     @Email
     private String email;
 
-    @NotEmpty(message = "Cannot empty, setup if user is admin required")
+    @NotNull(message = "Cannot null, setup if user is admin required")
     private Boolean isUserAdmin;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getUserAdmin() {
+        return isUserAdmin;
+    }
+
+    public void setUserAdmin(Boolean userAdmin) {
+        isUserAdmin = userAdmin;
+    }
 }
