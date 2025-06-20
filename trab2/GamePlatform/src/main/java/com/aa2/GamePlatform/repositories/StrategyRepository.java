4f8d353d4cf581f37d26c1cfa6386ed241dabfe1
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-06-20 11:57:55 -0300

    feat: StrategyRepository

---
+++ trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/repositories/StrategyRepository.java
@@ -0,0 +1,7 @@
+package com.aa2.GamePlatform.repositories;
+
+import com.aa2.GamePlatform.models.Strategy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StrategyRepository extends JpaRepository<Strategy,Integer> {
+}
