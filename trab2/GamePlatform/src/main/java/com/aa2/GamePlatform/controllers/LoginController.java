655414879734e3372219bc16f89fb336ed69e9bf
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-06-23 08:45:21 -0300

    logoutOC

--- trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/controllers/LoginController.java
+++ trab2/GamePlatform/src/main/java/com/aa2/GamePlatform/controllers/LoginController.java
@@ -5,6 +5,7 @@ import com.aa2.GamePlatform.models.UserSession;
 import com.aa2.GamePlatform.repositories.TesterRepository;
 import com.aa2.GamePlatform.repositories.UserSessionRepository;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -52,6 +53,20 @@ public class LoginController {
 
         // Set session token as cookie
         Cookie cookie = new Cookie("SESSION_TOKEN", session.getToken());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(24 * 60 * 60); // 1 day
+        response.addCookie(cookie);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("logout")
+    public String logout(
+            HttpServletResponse response
+    ) {
+        // Set session token as cookie
+        Cookie cookie = new Cookie("SESSION_TOKEN", "");
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         cookie.setMaxAge(24 * 60 * 60); // 1 day
