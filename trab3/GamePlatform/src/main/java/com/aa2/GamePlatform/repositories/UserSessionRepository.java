4d6471abe2eb88b6d5b320037eb4265bbd6cba13
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-07-13 12:58:38 -0300

    removed old GamePlatform

--- trab3/GamePlatform/src/main/java/com/aa2/GamePlatform/repositories/UserSessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aa2.GamePlatform.repositories;
-
-import com.aa2.GamePlatform.models.UserSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSessionRepository extends JpaRepository<UserSession, Long> {
-    UserSession findByToken(String token);
-}
